f26dd18f230a1ff4dd89dbe51b2a9f05d4522de5
Muhammed Batuhan Bayram <muhammedbatuhanbayram@gmail.com>
2024-08-16 12:46:42 +0300

    loga yazma tamamlandı

---
+++ day1/day_1.js
@@ -0,0 +1,25 @@
+// Document write ile çıktı verme
+
+document.writeln("Hello World <br>"); // line (ln) alt satıra geç
+document.writeln("Batuhan Bayram");
+
+// console ile çıktı verme
+
+console.log("Batuhan Bayram");
+// console.clear(); //konsolu temizler
+
+let a=486
+let b=533
+
+console.log("İki Sayının Toplamı :" ,a+b);
+console.clear();
+
+// Alert (Uyarı) popup ile çıktı verme
+
+// console.log(window);
+
+
+alert("a ve b olmak üzere iki sayımız olsun. a=486 ve b=533 ise a ve b'yi toplayacak olursak sonuç: " + (a+b));
+
+console.error("Hata!");
+console.warn("Hata!");
